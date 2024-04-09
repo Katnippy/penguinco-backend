67c407bb831202338de60595d94b4fd3a6b297a0
Katnippy <katnippy@proton.me>
2024-04-09 16:37:01 +0100

    Create store and stock item entities.

---
+++ PenguinCo.Api/Entities/StockItem.cs
@@ -0,0 +1,7 @@
+﻿namespace PenguinCo.Api.Entities;
+
+public class StockItem
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
---
+++ PenguinCo.Api/Entities/Store.cs
@@ -0,0 +1,17 @@
+﻿namespace PenguinCo.Api.Entities;
+
+public class Stock
+{
+    public int StockItemId { get; set; }
+    public required StockItem StockItem { get; set; }
+    public int Quantity { get; set; }
+}
+
+public class Store
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Address { get; set; }
+    public List<Stock>? Stock { get; set; }
+    public DateOnly Updated { get; set; }
+}
